0ca1140ddfd1dedcde781b118999d9dc5ae94a64
MolnarArthur <arthur.molnar@usource.ro>
2019-03-07 10:40:59 +0200

    Change style dynamically

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
+const classArray = ["padded", "bigger", "borderAdd"];
+
+document.addEventListener('DOMContentLoaded', function() {
+    classArray.forEach(function(item) {
+        let opt = document.createElement('option');
+        opt.value = item;
+        opt.innerHTML = item;
+        selClasses.appendChild(opt);
+        console.log(opt);
+    })
+});
+
+const selClasses = document.querySelector('select[name=classes]');
+
 const selfElement = document.querySelector('div');
 const seInput = document.querySelector('input[name=outputText]');
 const seBg = document.querySelector('input[name=bg]');
