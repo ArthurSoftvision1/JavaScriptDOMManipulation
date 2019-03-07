5bf0fae79424f0870c313f237232424cdff38f56
MolnarArthur <arthur.molnar@usource.ro>
2019-03-07 10:53:58 +0200

    Create a main node of HTML elements

--- js/app.js
+++ js/app.js
@@ -6,12 +6,16 @@ document.addEventListener('DOMContentLoaded', function() {
         opt.value = item;
         opt.innerHTML = item;
         selClasses.appendChild(opt);
-        console.log(opt);
+    })
+    nodeLister = document.querySelectorAll('#main >*');
+    nodeLister.forEach(function(item) {
+        console.log(item);
     })
 });
 
 const selClasses = document.querySelector('select[name=classes]');
 
+let nodeLister;
 const selfElement = document.querySelector('div');
 const seInput = document.querySelector('input[name=outputText]');
 const seBg = document.querySelector('input[name=bg]');
